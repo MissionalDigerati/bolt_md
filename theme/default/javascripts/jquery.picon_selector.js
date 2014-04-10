4c1b9dd8a3f1bfc7fdab32786145a67b557ffffe
Codemis <johnathan@missionaldigerati.com>
2014-04-10 13:10:43 -0700

    Added an image picker for the icons.

--- theme/default/javascripts/jquery.picon_selector.js
+++ theme/default/javascripts/jquery.picon_selector.js
@@ -1,11 +1,21 @@
 jQuery(document).ready(function($) {
-    var picon = $('#tagline_icon').find(":selected").text();
-     changePicon(picon);
-     $('#tagline_icon').change(function(event) {
-        var picon = $(this).find("option:selected").text();
-         changePicon(picon);
-     });
+    $('#tagline_icon > option').each(function(index, el) {
+        var picon = $(this).text();
+        addPicon(picon, $(this).is(':selected'));
+    });
+    $('div.picon_select_holder').click(function(event) {
+        $('div.picon_select_holder').removeClass('selected');
+        $(this).addClass('selected');
+        var picon = $(this).attr('data-picon');
+        $('#tagline_icon option').filter(function() {
+            return $.trim($(this).text()) == picon; 
+        }).prop('selected', true);
+    });
 });
-function changePicon(picon) {
-    $('#picon_choice').html("<i class='icon-picons-" + picon + "' style='font-size: 40px;'></i>");
+function addPicon(picon, selected) {
+    var addClass = '';
+    if (selected === true) {
+        addClass = ' selected';
+    };
+    $('#picon_choice').append("<div class='picon_select_holder" + addClass + "' data-picon='" + picon + "'><i class='icon-picons-" + picon + " picon_select'></i></div>");
 };
