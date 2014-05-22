c285b5b9dda4e3a40c4be955a473e2cd1c9522bb
Codemis <johnathan@missionaldigerati.com>
2014-05-22 15:55:22 -0700

    Added ability to sort by date time.

--- app/extensions/ClassManager/assets/class_manager_scripts.js
+++ app/extensions/ClassManager/assets/class_manager_scripts.js
@@ -1,5 +1,31 @@
 $(function(){
-    var table = $("table").stupidtable();
+    var datetime_from_string = function(str){
+        var months = ["jan","feb","mar","apr","may","jun","jul",
+                      "aug","sep","oct","nov","dec"];
+        var pattern = "([a-zA-Z]{3})\\s*(\\d{2}),\\s*(\\d{4})\\s*@{1}\\s*(\\d{2}):{1}(\\d{2})\\s*([a-zA-Z]{2})";
+        var re = new RegExp(pattern);
+        var dateParts = re.exec(str).slice(1);
+
+        var year = dateParts[2];
+        var month = $.inArray(dateParts[0].toLowerCase(), months);
+        var day = dateParts[1];
+        var hour = dateParts[3];
+        var minutes = dateParts[4];
+        var meridian = dateParts[5];
+        if (meridian == "pm") {
+            hour = parseInt(hour)+12;
+        };
+        return new Date(year, month, day, hour, minutes);
+    };
+    var table = $("table").stupidtable({
+        "datetime":function(a,b) {
+
+          aDate = datetime_from_string(a);
+          bDate = datetime_from_string(b);
+
+          return aDate - bDate;
+        }
+    });
     table.on("aftertablesort", function (event, data) {
         var th = $(this).find("th");
         th.find(".arrow").remove();
