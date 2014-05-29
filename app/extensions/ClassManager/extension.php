7d9b928639f0c7625bb20e7192fab3c0c8e4237d
Codemis <johnathan@missionaldigerati.com>
2014-05-29 13:24:09 -0700

    Added ability to remove students from class.

--- app/extensions/ClassManager/extension.php
+++ app/extensions/ClassManager/extension.php
@@ -21,9 +21,11 @@ class Extension extends \Bolt\BaseExtension
      * @access private
      **/
     private $extensionPaths = array(
-        'roster'    =>  '',
-        'sign_in'   =>  '',
-        'branding'  =>  ''
+        'home'              =>  '',     
+        'roster'            =>  '',
+        'sign_in'           =>  '',
+        'branding'          =>  '',
+        'remove_student'    =>  ''
     );
 
     /**
@@ -87,6 +89,7 @@ class Extension extends \Bolt\BaseExtension
              * Add the button in the extensions navigation
              */
             $this->path = $this->extensionPaths['branding'] . '/extensions/class-manager';
+            $this->extensionPaths['home'] = $this->path;
             $this->app->match($this->path, array($this, 'classManagerLoad'));
             $this->addMenuOption(__('Manage Classes'), $this->app['paths']['bolt'] . 'extensions/class-manager', "icon-group");
         }
@@ -95,8 +98,14 @@ class Extension extends \Bolt\BaseExtension
          */
         $this->extensionPaths['roster'] = $this->extensionPaths['branding'] . '/extensions/class-manager/roster/';
         $this->app->match($this->extensionPaths['roster'] . "{class_slug}/{id}", array($this, 'classManagerLoadRoster'));
-        $this->extensionPaths['sign_in'] = $this->extensionPaths['branding'] . '/extensions/class-manager/sign_in_sheet/';
+        $this->extensionPaths['sign_in'] = $this->extensionPaths['branding'] . '/extensions/class-manager/sign-in-sheet/';
         $this->app->match($this->extensionPaths['sign_in'] . "{class_slug}/{id}", array($this, 'classManagerLoadSignInSheet'));
+        /**
+         * /class-manager/class/digerati_101/3/remove-student/1
+         */
+        $this->extensionPaths['remove_student'] = $this->extensionPaths['branding'] . '/extensions/class-manager/class/';
+        $this->app->match($this->extensionPaths['remove_student'] . "{class_slug}/{id}/remove-student/{student_id}", array($this, 'classManagerRemoveStudent'));
+
         /**
          * Add ClassManager template namespace to twig
          */
@@ -113,12 +122,14 @@ class Extension extends \Bolt\BaseExtension
     public function classManagerLoad()
     {
         $classes = $this->app['storage']->getContent('upcoming_classes', array('order' => 'date_of_class DESC'));
+        $deleteSuccess = $this->app['request']->get('delete_success');
 
         $body = $this->app['render']->render('@ClassManager/base.twig',
             array(
-                'classes'       =>  $classes,
-                'roster_path'   =>  $this->extensionPaths['roster'],
-                'sign_in_path'  =>  $this->extensionPaths['sign_in']
+                'classes'           =>  $classes,
+                'roster_path'       =>  $this->extensionPaths['roster'],
+                'delete_success'    =>  $deleteSuccess,
+                'sign_in_path'      =>  $this->extensionPaths['sign_in']
             )
         );
 
@@ -128,9 +139,10 @@ class Extension extends \Bolt\BaseExtension
     /**
      * Load the Roster Page for the class
      *
+     * @param string $class_slug The slug for the given class
      * @param integer $id the id of the class to retrieve
      * @return Response \Symfony\Component\HttpFoundation\JsonResponse
-     * @access private
+     * @access public
      * @author Johnathan Pulos
      **/
     public function classManagerLoadRoster($class_slug = '', $id = null)
@@ -138,6 +150,7 @@ class Extension extends \Bolt\BaseExtension
         $id = (int) $id;
         $view = $this->getViewTemplate($class_slug);
         $table = $this->getClassTable($class_slug);
+        $deleteSuccess = $this->app['request']->get('delete_success');
 
         $sql = "Select * FROM $table WHERE class_id = ? ORDER BY name ASC";
         $students = $this->app['db']->fetchAll($sql, array($id));
@@ -150,9 +163,13 @@ class Extension extends \Bolt\BaseExtension
 
         $body = $this->app['render']->render("@ClassManager/$view",
             array(
-                'students'      =>  $students,
-                'class_record'  =>  $classRecord,
-                'is_sign_in'    =>  false
+                'students'              =>  $students,
+                'class_record'          =>  $classRecord,
+                'is_sign_in'            =>  false,
+                'training_type_slug'    =>  $class_slug,
+                'class_id'              =>  $id,
+                'delete_success'        =>  $deleteSuccess,
+                'remove_student_path'   =>  $this->extensionPaths['remove_student']
             )
         );
 
@@ -162,9 +179,10 @@ class Extension extends \Bolt\BaseExtension
     /**
      * Load the Sign In Sheet
      *
+     * @param string $class_slug The slug for the given class
      * @param integer $id the id of the class to retrieve
      * @return Response \Symfony\Component\HttpFoundation\JsonResponse
-     * @access private
+     * @access public
      * @author Johnathan Pulos
      **/
     public function classManagerLoadSignInSheet($class_slug = '', $id = null)
@@ -172,6 +190,7 @@ class Extension extends \Bolt\BaseExtension
         $id = (int) $id;
         $view = $this->getViewTemplate($class_slug);
         $table = $this->getClassTable($class_slug);
+        $deleteSuccess = $this->app['request']->get('delete_success');
         
         $sql = "Select * FROM $table WHERE class_id = ? ORDER BY name ASC";
         $students = $this->app['db']->fetchAll($sql, array($id));
@@ -184,13 +203,68 @@ class Extension extends \Bolt\BaseExtension
 
         $body = $this->app['render']->render("@ClassManager/$view",
             array(
-                'students'      =>  $students,
-                'class_record'  =>  $classRecord,
-                'is_sign_in'    =>  true
+                'students'              =>  $students,
+                'class_record'          =>  $classRecord,
+                'training_type_slug'    =>  $class_slug,
+                'class_id'              =>  $id,
+                'is_sign_in'            =>  true,
+                'delete_success'        =>  $deleteSuccess,
+                'remove_student_path'   =>  $this->extensionPaths['remove_student']
             )
         );
 
         return new Response($this->injectAssets($body));
+    }
+
+    /**
+     * Remove a student from the class
+     *
+     * @param string $class_slug The slug for the given class
+     * @param integer $id the id of the class
+     * @param integer $id the id of the student to remove
+     * @return redirect
+     * @access public
+     * @author Johnathan Pulos
+     **/
+    public function classManagerRemoveStudent($class_slug = '', $id = null, $student_id = null)
+    {
+        $redirectPage = $this->app['request']->get('redirect');
+        $shouldRedirect = false;
+        $success = false;
+        if ($this->app['request']->getMethod() == 'POST') {
+            $method = $this->app['request']->get('_method');
+            if ($method != 'DELETE') {
+                $shouldRedirect = true;
+            }
+        } else {
+            $shouldRedirect = true;
+        }
+        if ($shouldRedirect === false) {
+            /**
+             * Remove the student, and send them back with a message
+             */
+            $id = (int) $id;
+            $student_id = (int) $student_id;
+            $view = $this->getViewTemplate($class_slug);
+            $table = $this->getClassTable($class_slug);
+            
+            $sql = "DELETE FROM $table WHERE class_id = ? AND id = ?";
+            $deleteQuery = $this->app['db']->executeQuery(
+                $sql,
+                array($id, $student_id),
+                array(\PDO::PARAM_INT, \PDO::PARAM_INT)
+            );
+            $success = true;
+        }
+        if ((isset($redirectPage) && ($redirectPage != ''))) {
+            $redirectTo = $this->extensionPaths[$redirectPage] . $class_slug . "/" . $id;
+        } else {
+            $redirectTo = $this->extensionPaths['home'];
+        }
+        if ($success === true) {
+            $redirectTo = $redirectTo . "?delete_success=true";
+        }
+        return $this->app->redirect($redirectTo);
     }
 
     /**
