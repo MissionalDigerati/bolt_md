d60d3218d05f30f03faeec6a8b9d0a735ccb4046
Codemis <johnathan@jpulos.com>
2015-05-11 09:45:09 -0700

    Upgraded Bolt CMS to the latest version.

--- app/extensions/PHPToolbox/readme.md
+++
@@ -1,9 +0,0 @@
-PHP Toolbox
-===========
-
-An extension that contains shortcut Twig functions for handling basic PHP functionality.
-
-Supported Functionality
------------------------
-
-* {{ md5('string') }} - med5 hash the given string
--- vendor/symfony/translation/Symfony/Component/Translation/Loader/PoFileLoader.php
+++ vendor/symfony/translation/Symfony/Component/Translation/Loader/PoFileLoader.php
@@ -19,7 +19,7 @@ use Symfony\Component\Config\Resource\FileResource;
  * @copyright Copyright (c) 2010, Union of RAD http://union-of-rad.org (http://lithify.me/)
  * @copyright Copyright (c) 2012, Clemens Tolboom
  */
-class PoFileLoader extends ArrayLoader implements LoaderInterface
+class PoFileLoader extends ArrayLoader
 {
     public function load($resource, $locale, $domain = 'messages')
     {
@@ -136,7 +136,6 @@ class PoFileLoader extends ArrayLoader implements LoaderInterface
                 $size = strpos($line, ']');
                 $item['translated'][(int) substr($line, 7, 1)] = substr($line, $size + 3, -1);
             }
-
         }
         // save last item
         $this->addMessage($messages, $item);
@@ -166,13 +165,13 @@ class PoFileLoader extends ArrayLoader implements LoaderInterface
                 end($plurals);
                 $count = key($plurals);
                 // Fill missing spots with '-'.
-                $empties = array_fill(0, $count+1, '-');
+                $empties = array_fill(0, $count + 1, '-');
                 $plurals += $empties;
                 ksort($plurals);
                 $messages[stripcslashes($item['ids']['plural'])] = stripcslashes(implode('|', $plurals));
             }
         } elseif (!empty($item['ids']['singular'])) {
-              $messages[stripcslashes($item['ids']['singular'])] = stripcslashes($item['translated']);
+            $messages[stripcslashes($item['ids']['singular'])] = stripcslashes($item['translated']);
         }
     }
 }
